4720aade2356b841d2151d7e1a3edd967596e290
Jiunn <jung_no_1@sina.cn>
2017-03-26 14:18:51 +0800

    添加注释

--- src/main/java/com/manment/bean/User.java
+++ src/main/java/com/manment/bean/User.java
@@ -2,6 +2,8 @@ package com.manment.bean;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 /**
@@ -80,7 +82,8 @@ public class User {
 	public void setAnswer(String answer) {
 		this.answer = answer;
 	}
-	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="Asia/Shanghai") 
+	@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")  
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")  
 	public Date getFreezingTime() {
 		return freezingTime;
 	}
